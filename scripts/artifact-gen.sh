d7d21d1ce59d8c0ca0482c8582541bc88d589f90
Fabio Ribeiro <faabiosr@gmail.com>
2025-05-03 21:51:50 +0200

    fix: testing zip
    using rsync

--- scripts/artifact-gen.sh
+++ scripts/artifact-gen.sh
@@ -11,5 +11,25 @@ cd "$MODULE_ROOT" || exit
 yarn
 yarn build
 
-# Generate artifact excluding everything that's not necessary for the correct operation of the module
-zip -r "$MODULE_ROOT/../WhiteLabel.zip" . -x ".git/*" ".husky/*" "node_modules/*" "scripts/*" ".commitlintrc.json" ".editorconfig" ".gitignore" ".versionrc.json" "postcss.config.js" "tailwind.config.js" "vite.config.js"
+#zip -r "$MODULE_ROOT/../WhiteLabel.zip" . -x ".git/*" ".github/*" "build/*" ".husky/*" "node_modules/*" "scripts/*" ".commitlintrc.json" ".editorconfig" ".gitignore" ".versionrc.json" "postcss.config.js" "tailwind.config.js" "vite.config.js"
+
+mkdir -p "$MODULE_ROOT/build/WhiteLabel"
+
+# copy required files only
+rsync -arh --no-links "$MODULE_ROOT/" \
+  --exclude 'build' \
+  --exclude '.commitlintrc.json' \
+  --exclude '.editorconfig' \
+  --exclude '.git' \
+  --exclude '.github' \
+  --exclude '.gitignore' \
+  --exclude '.husky' \
+  --exclude 'node_modules' \
+  --exclude 'postcss.config.js' \
+  --exclude '/scripts' \
+  --exclude 'tailwind.config.js' \
+  --exclude '.versionrc.json' \
+  --exclude 'vite.config.js' \
+  . "$MODULE_ROOT/build/WhiteLabel/"
+
+(cd "$MODULE_ROOT/build" && zip -r WhiteLabel.zip WhiteLabel)
